3ef2054a59648d713733718e74a24e1b0c56156a
benoit bessiere <benoit.bessiere@gmail.com>
2020-07-28 11:15:55 +0200

    start from scratch

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import { navSlide } from './src/js/navbar.js';
+import './src/scss/main.scss';
+// import AOS from 'aos';
+navSlide();
+// AOS.init({
+//   duration: 700, // values from 0 to 3000, with step 50ms
+//   easing: 'ease-out',
+// });
